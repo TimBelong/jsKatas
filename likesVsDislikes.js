11fc1b2be0b458557ae9a4979b5d03d333af01f4
inne <mrbelongtim@gmail.com>
2024-02-01 20:42:40 +0100

    resolved likes vs dislikes

--- likesVsDislikes.js
+++ likesVsDislikes.js
@@ -1,23 +1,9 @@
-function likeOrDislike(buttons) {
-    let likeButton = false;
-    let dislikeButton = false;
-
-    for (const button of buttons) {
-        if (button === "Like") {
-            likeButton = !likeButton && !dislikeButton;
-            dislikeButton = false;
-        } else if (button === "Dislike") {
-            dislikeButton = !dislikeButton && !likeButton;
-            likeButton = false;
-        }
-    }
-
-    if (likeButton)
-        return "Like";
-    else if (dislikeButton)
-        return "Dislike";
-    else
+const likeOrDislike = buttons => buttons.reduce((result, currentButton) => {
+    if (currentButton === result) {
         return "Nothing";
-}
+    } else {
+        return currentButton;
+    }
+}, "Nothing");
 
-console.log(likeOrDislike([Like,Like,Like]))
+console.log(likeOrDislike([Dislike,Like]))
